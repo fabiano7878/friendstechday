0cae78312a6add2a1eceddbd2859aa30cb9cfe9d
fabsilva <fabmarley78@hotmail.com>
2012-07-13 15:22:19 -0300

    Hello_Word_Egit

---
+++ br/com/HelloWord/Teste_Egit/Hello_Word.java
@@ -0,0 +1,15 @@
+package br.com.HelloWord.Teste_Egit;
+
+public class Hello_Word {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Fabiano Egit");
+		// TODO Auto-generated method stub
+
+	}
+
+}
